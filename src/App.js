d6b0fafb4638e05b2596646df635fb95182f59eb
Robbie <Robertb2342@gmail.com>
2021-06-25 17:51:02 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import TodoList from './Components/Todolist/Todolist'
 import UI from './Components/UI/UI'
+import Navbar from './Components/UI/Navbar'
 
 function App() {
   return ( <>
+      <Navbar />
       <TodoList />
       <UI />
     </>
---
+++ src/Components/UI/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Styling/Navbar.css'
+
+
+
+const Navbar = () => {
+    return(
+        <div className='navbar'>
+            <h2>Keeper App</h2>
+            <p>{}</p>
+        </div>
+    )
+}
+
+export default Navbar
